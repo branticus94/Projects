4852a33876fef0dbba64e17fb8eee5aa173ba3e9
Alexandra Howland <alexandra.e.brant@gmail.com>
2024-09-02 15:18:23 +0100

    added favourite python project

---
+++ Favourite/favourite.py
@@ -0,0 +1,4 @@
+# Take user input for favourite ice cream flavour printing the output
+favourite = input("What is your favourite ice cream flavour?")
+
+print("No way! I love " + favourite + " too!")
